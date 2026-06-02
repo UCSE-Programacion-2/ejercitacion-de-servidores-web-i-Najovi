7de8e95ef010d44f72d7e967301e494b55fc44c4
Najovi <vilcajorge0327@gmail.com>
2026-06-02 19:48:20 -0300

    Practico Finalizado

--- src/server.js
+++ src/server.js
@@ -7,9 +7,27 @@ const http = require('http');
  * Crea un servidor HTTP que utiliza la función router para manejar las solicitudes
  * @returns {http.Server} Instancia del servidor HTTP
  */
+
 function createServer() {
   const server = http.createServer((req, res) => {
     /* Aquí la lógica del enrutador */
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    if (req.url === '/') {
+      res.writeHead(200);
+      res.end('Bienvenid@s! Gracias por tu visita.');
+    } else if (req.url === '/nosotros') {
+      res.writeHead(200);
+      res.end('Bienvenid@s a saber + de nosotros :)');
+    } else if (req.url === '/cursos') {
+      res.writeHead(200);
+      res.end('Te interesan nuestros cursos. Vení a conocer la oferta!');
+    } else if (req.url === '/contacto') {
+      res.writeHead(200);
+      res.end('Si querés contactarnos, hacelo a este Email: :)');
+    } else {
+      res.writeHead(404);
+      res.end('No se ha encontrado la ruta ingresada.');
+    }
   });
 
   return server;
